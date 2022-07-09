e7aa78f31273987c268ec58453a24990bb1f4491
andrea.montoya@ed.company <terceiro-aruiz@icatuseguros.com.br>
2022-07-09 19:50:30 -0300

    trabajando con tipo de variables

---
+++ Carro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicosCPracticas
+{
+    public class Carro
+    {
+        public string Modelo = "Fox";
+        public string Marca = "Volkswagen";
+        public string Chassi = "HHHGGUU";
+        public string Fabricante = "Alemania";
+
+        public void Encender()
+        {
+            Console.WriteLine("Carro encendido!");
+        }
+
+        public void Apagar()
+        {
+            Console.WriteLine("Carro apagado!");
+        }
+
+        public void Acelerar()
+        {
+            Console.WriteLine("Carro acelerando...");
+        }
+
+        public void Frenar()
+        {
+            Console.WriteLine("Carro frenando...");
+        }
+    }
+
+
+}
+
+
--- Program.cs
+++ Program.cs
@@ -53,26 +53,38 @@
         //Console.Write(" char: " + c);
         //Console.WriteLine(" string: " + str);
 
-        int NUM1, NUM2, RESUL;
-        string linea;
-        Console.Write("PRIMER NÚMERO :");
-        linea = Console.ReadLine();
-        NUM1 = int.Parse(linea);
-        Console.Write("SEGUNDO NÚMERO :");
-        linea = Console.ReadLine();
-        NUM2 = int.Parse(linea);
-        Console.WriteLine();
-        RESUL = NUM1 + NUM2;
-        Console.WriteLine("LA SUMA ES {0}: ", RESUL);
-        RESUL = NUM1 - NUM2;
-        Console.WriteLine("LA RESTA ES: {0} - {1} = {2} ", NUM1, NUM2, RESUL);
-        RESUL = NUM1 * NUM2;
-        Console.WriteLine("LA MULTIPLICACIÓN ES: " + RESUL);
-        RESUL = NUM1 / NUM2;
-        Console.WriteLine("LA DIVISIÓN ES: " + RESUL);
-        RESUL = NUM1 % NUM2;
-        Console.WriteLine("EL RESIDUO ES: " + RESUL);
-        Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        //int NUM1, NUM2, RESUL;
+        //string linea;
+        //Console.Write("PRIMER NÚMERO :");
+        //linea = Console.ReadLine();
+        //NUM1 = int.Parse(linea);
+        //Console.Write("SEGUNDO NÚMERO :");
+        //linea = Console.ReadLine();
+        //NUM2 = int.Parse(linea);
+        //Console.WriteLine();
+        //RESUL = NUM1 + NUM2;
+        //Console.WriteLine("LA SUMA ES {0}: ", RESUL);
+        //RESUL = NUM1 - NUM2;
+        //Console.WriteLine("LA RESTA ES: {0} - {1} = {2} ", NUM1, NUM2, RESUL);
+        //RESUL = NUM1 * NUM2;
+        //Console.WriteLine("LA MULTIPLICACIÓN ES: " + RESUL);
+        //RESUL = NUM1 / NUM2;
+        //Console.WriteLine("LA DIVISIÓN ES: " + RESUL);
+        //RESUL = NUM1 % NUM2;
+        //Console.WriteLine("EL RESIDUO ES: " + RESUL);
+        //Console.Write("Pulse una Tecla:"); Console.ReadLine();
 
+        double SUELDO, ARECIBIR;
+        int NHIJO, BONO;
+        string NOM, linea;
+        Console.Write("NOMBRE EMPLEADO :"); NOM = Console.ReadLine();
+        Console.Write("SUELDO EMPLEADO :"); linea = Console.ReadLine();
+        SUELDO = double.Parse(linea);
+        Console.Write("NÚMERO DE HIJOS :"); linea = Console.ReadLine();
+        NHIJO = int.Parse(linea);
+        if (NHIJO >= 3) { BONO = NHIJO * 10; } else { BONO = NHIJO * 20; }
+        ARECIBIR = SUELDO + BONO;
+        Console.WriteLine("RECIBE :" + String.Format("{0:c}", ARECIBIR));
+        Console.Write("Pulse una Tecla:"); Console.ReadLine();
     }
 }
