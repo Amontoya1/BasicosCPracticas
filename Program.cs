b2eb1747e51a20d1c111ed5c05d11908b40cfc77
andrea.montoya@ed.company <terceiro-aruiz@icatuseguros.com.br>
2022-07-09 23:41:08 -0300

    dias de la semana practica de bucles

--- Program.cs
+++ Program.cs
@@ -224,17 +224,64 @@
 
         //Multiplicar com While
 
-        int NUM, I, RESUL;
+        //int NUM, I, RESUL;
+        //string linea;
+        //Console.Write("DIGITE NÚMERO:"); linea = Console.ReadLine();
+        //NUM = int.Parse(linea);
+        //I = 1;
+        //while ((I <= 12))
+        //{
+        //    RESUL = NUM * I;
+        //    Console.WriteLine("{0} * {1} = {2}", NUM, I, RESUL);
+        //    I = I + 1;
+        //}
+        //Console.Write("Pulse una Tecla:"); Console.ReadLine();
+
+
+        //Dias de la semana
+        int num;
         string linea;
-        Console.Write("DIGITE NÚMERO:"); linea = Console.ReadLine();
-        NUM = int.Parse(linea);
-        I = 1;
-        while ((I <= 12))
+        Console.WriteLine();
+        Console.WriteLine("DIAS DE LA SEMANA");
+        Console.WriteLine();
+        Console.Write("Ingrese un numero del 1 al 7 :");
+        linea = Console.ReadLine();
+        num = int.Parse(linea);
+        switch (num)
         {
-            RESUL = NUM * I;
-            Console.WriteLine("{0} * {1} = {2}", NUM, I, RESUL);
-            I = I + 1;
+            case 1:
+                Console.WriteLine();
+                Console.WriteLine("El numero que ingreso corresponde al dia DOMINGO");
+                break;
+            case 2:
+                Console.WriteLine();
+                Console.WriteLine("El numero que ingreso corresponde al dia LUNES");
+                break;
+            case 3:
+                Console.WriteLine();
+                Console.WriteLine("El numero que ingreso corresponde al dia MARTES");
+                break;
+            case 4:
+                Console.WriteLine();
+                Console.WriteLine("El numero que ingreso corresponde al dia MIERCOLES");
+                break;
+            case 5:
+                Console.WriteLine();
+                Console.WriteLine("El numero que ingreso corresponde al dia JUEVES");
+                break;
+            case 6:
+                Console.WriteLine();
+                Console.WriteLine("El numero que ingreso corresponde al dia VIERNES");
+                break;
+            case 7:
+                Console.WriteLine();
+                Console.WriteLine("El numero que ingreso corresponde al dia SABADO");
+                break;
+            default:
+                Console.WriteLine();
+                Console.WriteLine("El numero que ingreso esta fuera de rango");
+                break;
         }
-        Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        Console.ReadKey();
     }
 }
